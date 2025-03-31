30cf2c2746494d6a2ca45771f6df09edf9eabea9
marco76tv <marco.sottana@gmail.com>
2025-03-31 16:50:15 +0200

    .

--- laravel/Modules/Patient/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Patient\Http\Controllers\PatientController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('patient', PatientController::class)->names('patient');
-});
