6ff0f9ee0da0f852cf7abf171c74d3041323b9b8
marco76tv <marco.sottana@gmail.com>
2025-04-10 18:56:00 +0200

    Squashed 'laravel/Modules/Job/' changes from 6a169dea..ace82f2d
    ace82f2d Repository rebuilt
    REVERT: 6a169dea up
    
    git-subtree-dir: laravel/Modules/Job
    git-subtree-split: ace82f2d7d7e6cebd8abe3c1d9ad0bee0320b4ba

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -51,6 +51,10 @@ use Webmozart\Assert\Assert;
  * @property \Illuminate\Database\Eloquent\Collection<int, \Modules\Job\Models\Result> $results
  * @property int|null $results_count
  * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+<<<<<<< HEAD
+=======
+ *
+>>>>>>> origin/dev
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task query()
@@ -80,7 +84,13 @@ use Webmozart\Assert\Assert;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereTimezone($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereUpdatedBy($value)
+<<<<<<< HEAD
  * @property-read \Modules\Broker\Models\Profile|null $creator
+=======
+ *
+ * @property-read \Modules\Broker\Models\Profile|null $creator
+ *
+>>>>>>> origin/dev
  * @mixin \Eloquent
  */
 class Task extends BaseModel
@@ -90,27 +100,7 @@ class Task extends BaseModel
     use HasFactory;
     use Notifiable;
 
-    /**
-     * Compila i parametri del task per l'esecuzione.
-     *
-     * @param bool $forScheduler Se true, i parametri vengono formattati per lo scheduler
-     * @return array<int, string>|string
-     */
-    public function compileParameters(bool $forScheduler = false): array|string
-    {
-        if (null === $this->parameters) {
-            return [];
-        }
-
-        $parameters = \Safe\json_decode($this->parameters, true);
-        Assert::isArray($parameters);
-
-        if ($forScheduler) {
-            return array_map(fn ($value) => is_bool($value) ? ($value ? '1' : '0') : (string) $value, $parameters);
-        }
-
-        return $parameters;
-    }
+    /** @var list<string> */
     protected $fillable = [
         'id',
         'description',
