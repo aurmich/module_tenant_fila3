b6ad95ca3d2ea4edb2379b191619e015911bfdde
marco76tv <marco.sottana@gmail.com>
2025-04-10 16:35:30 +0200

    Add laravel/Modules/Lang_bak

--- bashscripts/git_pull_subtrees.sh
+++ bashscripts/git_pull_subtrees.sh
@@ -18,7 +18,7 @@ for ((i=0; i<total; i++)); do
     echo "---------"
     echo "Submodule $i:"
     echo "  📁 Path: $path"
-    echo "  URL: $url"
+    echo "  🌐 URL: $url"
     script="$script_dir/git_pull_subtree.sh"
     chmod +x "$script"
     sed -i -e 's/\r$//' "$script"
