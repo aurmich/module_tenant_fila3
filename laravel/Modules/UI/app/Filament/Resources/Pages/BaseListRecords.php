8f353fe8b99c533e76f4322b75fca74dd1e0d9b5
marco76tv <marco.sottana@gmail.com>
2025-04-10 22:05:08 +0200

    Add laravel/Modules/Tenant_bak

--- laravel/Modules/UI/app/Filament/Resources/Pages/BaseListRecords.php
+++ laravel/Modules/UI/app/Filament/Resources/Pages/BaseListRecords.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\UI\app\Filament\Resources\Pages;
 
-use Modules\UI\Traits\TableLayoutTrait;
+use Modules\UI\app\Traits\TableLayoutTrait;
 
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
