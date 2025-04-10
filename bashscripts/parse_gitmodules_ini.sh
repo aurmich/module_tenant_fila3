958076458b4f4be6bcb9a3991d8da7e8982bd654
marco76tv <marco.sottana@gmail.com>
2025-04-10 15:58:19 +0200

    Add laravel/Modules/Lang_bak

--- bashscripts/parse_gitmodules_ini.sh
+++ bashscripts/parse_gitmodules_ini.sh
@@ -32,7 +32,7 @@ while IFS= read -r line; do
         current_url="${BASH_REMATCH[1]}"
         
         # Chiamata esterna allo script di sincronizzazione
-        submodules_array[$index][]
+        submodules_array[$index]['path']=
         ((index++))
         
         # Pulizia: reset delle variabili per il prossimo modulo
